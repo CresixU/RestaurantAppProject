8d99cb7417efe3b7e1476d2084f4f3cc9727d0d9
Krystian Wawruch <CresixU@gmail.com>
2022-11-07 20:59:23 +0100

    Created orders

--- RestaurantAppProject/Models/Order.cs
+++ RestaurantAppProject/Models/Order.cs
@@ -6,9 +6,18 @@ namespace RestaurantAppProject.Models
     internal class Order
     {
         public Guid Id { get; set; }
-        public List<Item> items = new List<Item>();
+        public List<Item> Items = new List<Item>();
         public DateTime OrderTime { get; set; }
         public decimal Price { get; set; }
-        public Person Owner { get; set; }
+        public Person? Owner { get; set; }
+
+        public Order(List<Item> items, decimal price, Person owner)
+        {
+            Id = Guid.NewGuid();
+            Items = items;
+            OrderTime = DateTime.Now;
+            Price = price;
+            Owner = owner;
+        }
     }
 }
---
+++ RestaurantAppProject/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using RestaurantAppProject.Models;
+using RestaurantAppProject.Models.People;
+using RestaurantAppProject.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAppProject.Services
+{
+    internal class OrderService
+    {
+        public List<Order> orders = new List<Order>();
+
+        public void Create(List<Item> items, decimal price,Person owner)
+        {
+
+            orders.Add(new Order(items, price, owner));
+        }
+    }
+}
